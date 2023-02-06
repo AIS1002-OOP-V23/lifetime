857a59c8cef4bac10ffd05632de2097032bc8a4f
Lars Ivar Hatledal <laht@ntnu.no>
2023-02-06 10:40:45 +0100

    raii example

--- main.cpp
+++ main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
+class MyClass {
+
+public:
+    MyClass(const std::string& path) {
+
+        if_.open(path);
+    }
+
+    std::string readLine() {
+        std::string line;
+        std::getline(if_, line);
+
+        return line;
+    }
+
+    ~MyClass() {
+        if_.close();
+    }
+
+private:
+    std::ifstream if_;
+
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    {
+        MyClass fileReader("CMakeCache.txt"); // object set ups and manages the file resource
+        std::cout << fileReader.readLine() << std::endl;
+    } // destructor is called, file is closed
+
+
     return 0;
 }
